f4d8930ce16b3f51cf5a3f20f38b4d0a76a19c38
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-04-30 17:48:57 +0530

    style header and footer

--- src/script.js
+++ src/script.js
@@ -5,11 +5,27 @@ document.querySelector("#color").addEventListener("input", (e) => {
     fetch(`https://www.thecolorapi.com/id?hex=${hex}&format=json`)
         .then(res=>res.json())
         .then(data => {
-            document.querySelector("#scheme").innerHTML +=`
+            document.querySelector("#scheme").innerHTML =`
             <option selected>${data.name.value}</option>        
         `
         })
 });
+
+document.querySelector("#generate").addEventListener("click", (e) => {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&format=json&mode=analogic&count=5`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            const container=document.querySelector(".color-img")
+            container.innerHTML =``
+            for(let i=0;i<5;i++){
+                container.innerHTML +=`
+                    <img src="${data.colors[i].image.bare}" alt="color-img"/>
+                `
+            }
+
+        })
+})
 
 
 
